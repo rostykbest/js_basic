aebecd44ad0ef31f88ac1d9558fbd803fc89f323
rstan <rostykbest@gmail.com>
2023-08-25 15:03:42 +0300

    lesson1

---
+++ lesson1/1.js
@@ -0,0 +1,8 @@
+//let x = prompt("Enter days");
+//let y = x * 86400;
+//alert("The quantity of the seconds in " + x + " days is " + y);
+
+let dayQuantity = +prompt("How many days?");
+let result =
+  "There are " + dayQuantity * 86400 + " seconds in " + dayQuantity + " day(s)";
+alert(result);
